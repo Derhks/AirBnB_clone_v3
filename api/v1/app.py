9681213193782e170bf4f6c22e8620d278130c8d
Derhks <derhks@gmail.com>
2020-04-29 23:49:19 +0000

    [Create] register a blueprint

---
+++ api/v1/app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from models import storage
+from api.v1.views import app_views
+from os import environ
+from flask import Flask, Blueprint
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def close(code):
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host=environ.get('HBNB_API_HOST'),
+            port=environ.get('HBNB_API_PORT'),
+            debug=True)
