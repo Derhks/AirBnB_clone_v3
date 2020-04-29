b987f1a181aa25c7b1ad62851c266e374ecfc977
Derhks <derhks@gmail.com>
2020-04-29 23:48:21 +0000

    [Create] a file that return the status

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify
+from flask import Flask, jsonify
 
 
 @app_views.route('/status', strict_slashes=False)
