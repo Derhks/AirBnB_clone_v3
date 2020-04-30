be9cd3808d4b262d47fe1841fd35fa354d753852
Derhks <derhks@gmail.com>
2020-04-30 02:10:22 +0000

    [Update] new feature, errorhandler 404 - json

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,8 @@
 from models import storage
 from api.v1.views import app_views
 from os import environ
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, jsonify, make_response
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -15,6 +16,11 @@ def close(code):
     This method close the storage
     """
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 if __name__ == "__main__":
     app.run(host=environ.get('HBNB_API_HOST'),
